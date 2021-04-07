3f2ca6f2083c65930a8fe8375a6786cceaea57ea
AymenSb <73080747+AymenSBt@users.noreply.github.com>
2021-04-07 23:43:11 +0100

    commit

---
+++ app/Http/Controllers/MachinesAttachmentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MachinesAttachments;
+use Illuminate\Http\Request;
+
+class MachinesAttachmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MachinesAttachments  $machinesAttachments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MachinesAttachments $machinesAttachments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MachinesAttachments  $machinesAttachments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MachinesAttachments $machinesAttachments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MachinesAttachments  $machinesAttachments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MachinesAttachments $machinesAttachments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MachinesAttachments  $machinesAttachments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MachinesAttachments $machinesAttachments)
+    {
+        //
+    }
+}
