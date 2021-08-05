1736d5a55802f90b46e0cf7282d92aa53214627e
AymenSb <73080747+AymenSBt@users.noreply.github.com>
2021-08-05 21:06:55 +0100

    initial release

--- database/migrations/2021_04_17_193722_create_ads_table.php
+++ database/migrations/2021_04_17_193722_create_ads_table.php
@@ -16,7 +16,10 @@ class CreateAdsTable extends Migration
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('title');
+            $table->string('sub_title');
             $table->string('file_name');
+            $table->longText('base64Url');
             $table->boolean('is_Visible')->default(false);
             $table->softDeletes();
             $table->timestamps();
