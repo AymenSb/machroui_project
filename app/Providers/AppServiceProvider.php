540dba7aef1dbf39ecdae361bbe35276008d54d1
AymenSb <73080747+AymenSBt@users.noreply.github.com>
2021-08-11 11:16:51 +0100

    kk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,10 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS','on'); // this line
-    
+        
             URL::forceSchema('https');
-        }
+        
     }
 }
